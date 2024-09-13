de9a1999d63cc2b30511894310d011eb5173f77d
Dallas Olsen <Dolse1988@hotmail.com>
2024-09-12 19:01:01 -0600

    updated

---
+++ python_fundamentals.py
@@ -0,0 +1,11 @@
+# This is a comment a comment is not executed
+
+# variable is a word that stores information
+# loosely type DATA TYPE
+#      STRING -> chars  = Dallas  5rFtz
+#      int integer 1 3 4 5678
+#      float decimal number 1.0.12.4.4.5678
+username = 'Dallas'
+a_number = 4 #datatype is int
+a_float = 4.6 # datatype a float
+string_number = '4' # a string datatype
